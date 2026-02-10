4efb65be9d77ff8efd198feacc1a147c078c7582
avi9me <9tehri@gmail.com>
2026-02-10 15:42:21 +0530

    Change expected title in example.spec.ts test

--- UiTests/tests/example.spec.ts
+++ UiTests/tests/example.spec.ts
@@ -2,5 +2,5 @@ import { test, expect } from '@playwright/test';
 
 test('Example.com homepage loads', async ({ page }) => {
   await page.goto('https://example.com');
-  await expect(page).toHaveTitle(/Example Domain/);
+  await expect(page).toHaveTitle(/Google/);
 });
