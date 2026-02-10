d17cc7b6b87b81e4f90cf27fb367556de7d05834
avi9me <9tehri@gmail.com>
2026-02-10 15:34:03 +0530

    Fix URL in homepage load test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
 test('Example.com homepage loads', async ({ page }) => {
-  await page.goto('https://example.com');
+  await page.goto('https://example.co');
   await expect(page).toHaveTitle(/Example Domain/);
 });
