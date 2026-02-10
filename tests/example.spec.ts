a602c119c779b26c5737ca91adff07e260c4fde4
avi9me <9tehri@gmail.com>
2026-02-10 15:37:12 +0530

    Fix URL in example.spec.ts test case

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
 test('Example.com homepage loads', async ({ page }) => {
-  await page.goto('https://example.co');
+  await page.goto('https://example.com');
   await expect(page).toHaveTitle(/Example Domain/);
 });
