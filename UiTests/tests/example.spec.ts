ec669c74f924085e916c7356bc975ee6921e20db
avi9me <9tehri@gmail.com>
2026-02-10 15:46:18 +0530

    Update test for Example.com homepage title

--- UiTests/tests/example.spec.ts
+++ UiTests/tests/example.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
 test('Example.com homepage loads', async ({ page }) => {
-  await page.goto('https://example.com');
-  await expect(page).toHaveTitle(/Google/);
+  await page.goto('https://example.com/helloji');
+  await expect(page).toHaveTitle(/Example Domain/);
 });
