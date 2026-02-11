0fe99687d87bb236b49b4b00d3c1bea13effda10
avi9me <9tehri@gmail.com>
2026-02-11 15:29:49 +0530

    Update URL in example.spec.ts for homepage test

--- UiTests/tests/example.spec.ts
+++ UiTests/tests/example.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('Example.com homepage loads', async ({ page }) => {
-  await page.goto('https://example.com/helloji');
+  await page.goto('https://example.com');
   await expect(page).toHaveTitle(/Example Domain/);
 });
 
